fb89e14d5a25314e8da0715634e23631e94bdb9a
Edgar A. Bering IV <trizor@gmail.com>
2019-01-17 19:10:04 -0500

    Enable god Extra in describe_god_with_join
    The describe_god_with_join pane has a distinct implementation from the
    standard describe_god ui, which assumed that there were always only 3
    god panes. This is generalized to support displaying the Extra pane when
    kneeling at an altar if relevant.

--- crawl-ref/source/describe-god.cc
+++ crawl-ref/source/describe-god.cc
@@ -1276,8 +1276,6 @@ bool describe_god_with_join(god_type which_god)
         more_sw->add_child(make_shared<Text>(prompt_fs));
     }
 
-    const int num_panes = 3; // overview, detailed, wrath
-
     join_step_type step = SHOW;
     bool yesno_only = false;
     bool done = false, join = false;
@@ -1293,7 +1291,7 @@ bool describe_god_with_join(god_type which_god)
             return done = true;
         if (keyin == '!' || keyin == CK_MOUSE_CMD || keyin == '^')
         {
-            int n = (desc_sw->current() + 1) % num_panes;
+            int n = (desc_sw->current() + 1) % desc_sw->num_children();
             desc_sw->current() = n;
 #ifdef USE_TILE_WEB
             tiles.json_open_object();
@@ -1346,7 +1344,7 @@ update_ui:
         tiles.json_write_int("pane", desc_sw->current());
         tiles.ui_state_change("describe-god", 0);
 #endif
-        more_sw->current() = num_panes + step*2 + yesno_only;
+        more_sw->current() = desc_sw->num_children() + step*2 + yesno_only;
         return true;
     });
 

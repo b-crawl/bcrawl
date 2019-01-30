9129a1f489da16a992224059289f6e9007f2ed80
advil <rawlins@gmail.com>
2019-01-29 22:18:59 -0500

    Use a separate rng for ghosts
    The most recent set of seed instabilities I've been seeing are because
    the ghost loading code makes a different number of random number draws
    depending on whether it loads from the permastore or not, and in a clean
    setup it was guaranteed to draw from the permastore on the first run,
    but not thereafter, for some seeds. (Because even a permastore ghost
    gets resaved as an ephemeral ghost if a level is vetoed - something that
    is perhaps a bug itself.) In general, it seems risky to use levelgen rng
    for something that may depend on what's on a particular file system, so
    separate this off into its own rng for cases like this, distinct also
    from the gameplay rng.
    
    Also, fixed a bug in loading of ephemeral ghost files where if there was
    more than one ghost in the file, the wrong one would be removed after a
    ghost was selected for use.

--- crawl-ref/source/files.cc
+++ crawl-ref/source/files.cc
@@ -2136,6 +2136,8 @@ static vector<ghost_demon> _load_permastore_ghosts(bool backup_on_upgrade=false)
  */
 bool define_ghost_from_bones(monster& mons)
 {
+    rng_generator rng(RNG_SYSTEM_SPECIFIC);
+
     bool used_permastore = false;
 
     vector<ghost_demon> loaded_ghosts = _load_ephemeral_ghosts();
@@ -2165,7 +2167,7 @@ bool define_ghost_from_bones(monster& mons)
 
     if (!used_permastore)
     {
-        loaded_ghosts.erase(loaded_ghosts.begin());
+        loaded_ghosts.erase(loaded_ghosts.begin() + place_i);
 
         if (!loaded_ghosts.empty())
             save_ghosts(loaded_ghosts);
@@ -2697,6 +2699,7 @@ static size_t _ghost_permastore_size()
 
 static vector<ghost_demon> _update_permastore(const vector<ghost_demon> &ghosts)
 {
+    rng_generator rng(RNG_SYSTEM_SPECIFIC);
     if (ghosts.empty())
         return ghosts;
 

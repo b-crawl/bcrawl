271de0e287f85190d5daac4dd61aa1a22d9b3efd
bhauth <b@name.com>
2019-07-18 17:14:58 -0600

    hide Vine Stalker evolution apts

--- crawl-ref/source/util/gen-apt.pl
+++ crawl-ref/source/util/gen-apt.pl
@@ -129,6 +129,9 @@ sub aptitude_table
         next if $sp eq 'Lava Orc';
         next if $sp eq 'Tengu';
         next if $sp eq 'Dusk Walker';
+        next if $sp eq 'Mana Stalker';
+        next if $sp eq 'Twilight Stalker';
+        next if $sp eq 'Jungle Stalker';
 
         my $line = '';
         $line .= fix_draco_species($sp, \$seen_draconian_length);

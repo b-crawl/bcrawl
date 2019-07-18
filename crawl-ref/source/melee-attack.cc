2030f43ee0e34227f01c29ca7db8f14bee47ebfb
bhauth <b@name.com>
2019-07-18 17:11:57 -0600

    reorder bite brand priority

--- crawl-ref/source/melee-attack.cc
+++ crawl-ref/source/melee-attack.cc
@@ -1164,14 +1164,14 @@ public:
 
     int get_brand() const override
     {
+        if (you.get_mutation_level(MUT_ACIDIC_BITE))
+            return SPWPN_ACID;
         if (you.get_mutation_level(MUT_ANTIMAGIC_BITE))
             return SPWPN_ANTIMAGIC;
         if (you.get_mutation_level(MUT_POISON_BITE))
             return SPWPN_VENOM;
         if (you.get_mutation_level(MUT_DRAIN_BITE))
             return SPWPN_DRAINING;
-        if (you.get_mutation_level(MUT_ACIDIC_BITE))
-            return SPWPN_ACID;
 
         return SPWPN_NORMAL;
     }

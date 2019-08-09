b63c4b96a7c97da6f2b3706c81faba32959b05f9
bhauth <b@name.com>
2019-08-09 01:52:16 -0600

    more Frederick adjustments

--- crawl-ref/source/mon-spell.h
+++ crawl-ref/source/mon-spell.h
@@ -2223,7 +2223,7 @@ static const mon_spellbook mspell_list[] =
        { SPELL_SEAL_DOORS, 50, MON_SPELL_WIZARD },
        { SPELL_IRON_SHOT, 100, MON_SPELL_WIZARD },
        { SPELL_DEFLECT_MISSILES, 14, MON_SPELL_WIZARD },
-       { SPELL_MAJOR_DESTRUCTION, 50, MON_SPELL_NATURAL },
+       { SPELL_MAJOR_DESTRUCTION, 40, MON_SPELL_NATURAL },
       }
     },
     // ('&', mostly) Demon lords.

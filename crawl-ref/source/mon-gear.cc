9e17e904027bae9c0c8efa7b8121c2ed7ee51e45
bhauth <b@name.com>
2019-11-07 19:35:04 -0700

    more armor for Margery

--- crawl-ref/source/mon-gear.cc
+++ crawl-ref/source/mon-gear.cc
@@ -1942,8 +1942,10 @@ int make_mons_armour(monster_type type, int level)
 
     case MONS_MARGERY:
         item.base_type = OBJ_ARMOUR;
-        item.sub_type = random_choose_weighted(3, ARM_ACID_DRAGON_ARMOUR,
-                                               1, ARM_SWAMP_DRAGON_ARMOUR,
+        item.sub_type = random_choose_weighted(2, ARM_SHADOW_DRAGON_ARMOUR,
+                                               2, ARM_QUICKSILVER_DRAGON_ARMOUR,
+                                               2, ARM_STORM_DRAGON_ARMOUR,
+                                               3, ARM_GOLD_DRAGON_ARMOUR,
                                                6, ARM_FIRE_DRAGON_ARMOUR);
         break;
 

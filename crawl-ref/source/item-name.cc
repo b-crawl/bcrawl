bbbb819894eec9b1566b517a9cd6f62d56c6375c
gammafunk <gammafunk@gmail.com>
2019-11-29 21:00:18 -0600

    Fix outdated food logic when worshiping Fedhas
    These were relevant when the god accepted rations as a cost for some
    abilities, but this is no longer the case.
    
    Don't always have a food acquirement option when worshiping Fedhas.
    Also don't consider food always useful and always an interesting
    autoexplore target under Fedhas.

--- crawl-ref/source/item-name.cc
+++ crawl-ref/source/item-name.cc
@@ -3658,9 +3658,6 @@ bool is_useless_item(const item_def &item, bool temp)
                 return false;
         }
 
-        if (you_worship(GOD_FEDHAS) && item.is_type(OBJ_FOOD, FOOD_RATION))
-            return false;
-
         return true;
 
     case OBJ_CORPSES:

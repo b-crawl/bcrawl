dc0aeed4c65b3259f499dafc54e5bbbb810a588e
bhauth <b@name.com>
2019-12-13 17:47:07 -0700

    don't require visibility for stairs enemy check

--- crawl-ref/source/stairs.cc
+++ crawl-ref/source/stairs.cc
@@ -523,7 +523,7 @@ static level_id _travel_destination(const dungeon_feature_type how,
     }
     else if(going_up && feat_is_staircase(how) && one_chance_in(25)
         && !(player_in_branch(BRANCH_DUNGEON) && you.depth < 3)
-        && there_are_monsters_nearby(true, true, false))
+        && there_are_monsters_nearby(true, false, false))
     {
         mpr("As you climb the stairs, a rune flashes!");
         if (you.no_tele(true, true))

14d70eb493e88c4ab8e5523be58356d3f75d8030
bhauth <b@name.com>
2020-01-12 15:45:50 -0700

    fix Xtahua spell flag

--- crawl-ref/source/mon-spell.h
+++ crawl-ref/source/mon-spell.h
@@ -1853,7 +1853,7 @@ static const mon_spellbook mspell_list[] =
       {
        { SPELL_SEARING_BREATH, 60, MON_SPELL_NATURAL | MON_SPELL_BREATH | MON_SPELL_NOISY },
        { SPELL_CAUSE_FEAR, 20, MON_SPELL_WIZARD },
-       { SPELL_FIRE_STORM, 20, MON_SPELL_MAGICAL },
+       { SPELL_FIRE_STORM, 20, MON_SPELL_WIZARD },
       }
     },
 

e1317e6a3b002bb1a24f0f7d5b01a4ccc5da1506
bhauth <b@name.com>
2020-02-02 19:58:10 -0700

    allow GnAc

--- crawl-ref/source/ng-restr.cc
+++ crawl-ref/source/ng-restr.cc
@@ -49,10 +49,6 @@ static bool _banned_combination(job_type job, species_type species)
             return true;
         }
         break;
-    case SP_GNOLL:
-        if (job == JOB_ARCHAEOLOGIST)
-            return true;
-        break;
     case SP_DEEP_DWARF:
         if (job == JOB_REAVER)
             return true;

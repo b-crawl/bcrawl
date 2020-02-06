12452345002ee654d96f517e936a338b8679ddf6
bhauth <b@name.com>
2020-02-06 02:59:41 -0700

    fix spelunker tiles

--- crawl-ref/source/tilepick-p.cc
+++ crawl-ref/source/tilepick-p.cc
@@ -815,18 +815,13 @@ void tilep_job_default(int job, dolls_data *doll)
             parts[TILEP_PART_LEG]   = TILEP_LEG_BELT_REDBROWN;
             break;
 
-#if TAG_MAJOR_VERSION == 34
-        case JOB_STALKER:
-            parts[TILEP_PART_HELM]  = TILEP_HELM_HOOD_GREEN;
+        case JOB_SPELUNKER:
             parts[TILEP_PART_BODY]  = TILEP_BODY_LEATHER_JACKET;
             parts[TILEP_PART_LEG]   = TILEP_LEG_PANTS_SHORT_GRAY;
             parts[TILEP_PART_HAND1] = TILEP_HAND1_SWORD_THIEF;
             parts[TILEP_PART_HAND2] = TILEP_HAND2_BOOK_GREEN_DIM;
-            parts[TILEP_PART_ARM]   = TILEP_ARM_GLOVE_WRIST_PURPLE;
-            parts[TILEP_PART_CLOAK] = TILEP_CLOAK_GREEN;
             parts[TILEP_PART_BOOTS] = TILEP_BOOTS_MIDDLE_BROWN2;
             break;
-#endif
 
         case JOB_ASSASSIN:
             parts[TILEP_PART_HELM]  = TILEP_HELM_MASK_NINJA_BLACK;

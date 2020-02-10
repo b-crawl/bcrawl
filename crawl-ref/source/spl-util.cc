5212e3605628197e33e00fdd3c586c059cb3ec93
bhauth <b@name.com>
2020-02-10 13:38:15 -0700

    Flaming Arrows is useless for Felids

--- crawl-ref/source/spl-util.cc
+++ crawl-ref/source/spl-util.cc
@@ -1245,6 +1245,7 @@ string spell_uselessness_reason(spell_type spell, bool temp, bool prevent,
 #endif
     case SPELL_PORTAL_PROJECTILE:
     case SPELL_SPECTRAL_WEAPON:
+    case SPELL_FLAMING_ARROWS:
         if (you.species == SP_FELID)
             return "this spell is useless without hands.";
         break;

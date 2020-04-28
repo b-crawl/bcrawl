12335d7cc0f376b34bdc8d69e4adb4ebce581bbc
bhauth <b@name.com>
2020-04-28 03:14:56 -0600

    gourmand amulet: add Ghoul-style healing

--- crawl-ref/source/food.cc
+++ crawl-ref/source/food.cc
@@ -526,17 +526,14 @@ int prompt_eat_chunks(bool only_auto)
     return 0;
 }
 
-static const char *_chunk_flavour_phrase(bool likes_chunks)
+static const char *_chunk_flavour_phrase(bool likes_chunks, int gourmand)
 {
     const char *phrase = "tastes terrible.";
 
-    if (you.species == SP_GHOUL)
-        phrase = "tastes great!";
-    else if (likes_chunks)
+    if (likes_chunks)
         phrase = "tastes great.";
     else
     {
-        const int gourmand = you.duration[DUR_GOURMAND];
         if (gourmand >= GOURMAND_MAX)
         {
             phrase = one_chance_in(1000) ? "tastes like chicken!"
@@ -630,19 +627,26 @@ static void _eat_chunk(item_def& food)
     int nutrition     = _chunk_nutrition(likes_chunks);
     bool suppress_msg = false; // do we display the chunk nutrition message?
     bool do_eat       = false;
+    
+    int gourmand = 0;
+    if (you.species == SP_GHOUL)
+        gourmand = GOURMAND_MAX;
+    else
+        gourmand = min(you.duration[DUR_GOURMAND], GOURMAND_MAX);
 
     switch (chunk_effect)
     {
     case CE_CLEAN:
     {
-        if (you.species == SP_GHOUL)
+        int hp_amt = 1 + random2avg(5 + you.experience_level, 3);
+        hp_amt = (hp_amt * gourmand) / GOURMAND_MAX;
+        if (hp_amt)
         {
             suppress_msg = true;
-            const int hp_amt = 1 + random2avg(5 + you.experience_level, 3);
             _heal_from_food(hp_amt);
         }
 
-        mprf("This raw flesh %s", _chunk_flavour_phrase(likes_chunks));
+        mprf("This raw flesh %s", _chunk_flavour_phrase(likes_chunks, gourmand));
         do_eat = true;
         break;
     }

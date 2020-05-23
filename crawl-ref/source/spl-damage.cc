d19dbde3ef978f794ccd0eb9fee1529cf7c0514b
bhauth <b@name.com>
2020-05-22 22:43:32 -0600

    don't change lightning rod damage with spell cost changes

--- crawl-ref/source/spl-damage.cc
+++ crawl-ref/source/spl-damage.cc
@@ -2349,7 +2349,7 @@ spret cast_thunderbolt(actor *caster, int pow, coord_def aim, bool fail)
     fail_check();
 
     const int juice
-        = (spell_mana(SPELL_THUNDERBOLT) + charges) * LIGHTNING_CHARGE_MULT;
+        = (2 + charges) * LIGHTNING_CHARGE_MULT;
 
     dprf("juice: %d", juice);
 

5586b0676bc6f33cadb84af842165ce4eccfd2a1
Heinrich Ody <heinrich.ody@gmail.com>
2020-12-01 22:46:39 +0100

    Fix unbreathing bug
    Now, when it is checked whether an actor is immune to a cloud, for
    poison cloud we check for unbreathing.
    
    [Committer's note: resolves #1564]

--- crawl-ref/source/cloud.cc
+++ crawl-ref/source/cloud.cc
@@ -871,7 +871,7 @@ bool actor_cloud_immune(const actor &act, cloud_type type)
         case CLOUD_MEPHITIC:
             return act.res_poison() > 0 || act.is_unbreathing();
         case CLOUD_POISON:
-            return act.res_poison() > 0;
+            return act.res_poison() > 0 || act.is_unbreathing();
         case CLOUD_STEAM:
             if (!act.is_player())
                 return act.res_steam() > 0;

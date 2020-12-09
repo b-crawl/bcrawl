1f22781bd7b348c51150b3498256c24a6096eb5b
wheals <whealswithinwheals@gmail.com>
2020-12-09 08:23:07 +0200

    Don't let Maurice steal stuff being worn
    See
    https://crawl.kelbi.org/crawl/morgue/Prakerore/crash-Prakerore-20201124-060246.txt
    for a crash caused by this.

--- crawl-ref/source/delay.h
+++ crawl-ref/source/delay.h
@@ -198,10 +198,9 @@ public:
 
     /**
      *@return true if the delay involves using the item in this way (and
-     * therefore the item should not be messed with). If item is nullptr,
-     * returns whether the delay type ever uses items in this way.
+     * therefore the item should not be messed with).
      */
-    virtual bool is_being_used(const item_def* /*item*/, operation_types /*oper*/) const
+    virtual bool is_being_used(const item_def& /*item*/) const
     {
         return false;
     }
@@ -237,6 +236,11 @@ public:
     {
         return "armour_on";
     }
+
+    bool is_being_used(const item_def& item) const override
+    {
+        return &item == &equip;
+    }
 };
 
 class ArmourOffDelay : public Delay
@@ -266,6 +270,11 @@ public:
     {
         return "armour_on";
     }
+
+    bool is_being_used(const item_def& item) const override
+    {
+        return &item == &equip;
+    }
 };
 
 class JewelleryOnDelay : public Delay
@@ -283,6 +292,11 @@ public:
     const char* name() const override
     {
         return "jewellery_on";
+    }
+
+    bool is_being_used(const item_def& item) const override
+    {
+        return &item == &jewellery;
     }
 };
 
@@ -410,6 +424,11 @@ public:
     const char* name() const override
     {
         return "drop_item";
+    }
+
+    bool is_being_used(const item_def& item_) const override
+    {
+        return &item_ == &item;
     }
 };
 
@@ -695,6 +714,11 @@ public:
     const char* name() const override
     {
         return "blurry_vision";
+    }
+
+    bool is_being_used(const item_def& item) const override
+    {
+        return &item == &scroll;
     }
 };
 

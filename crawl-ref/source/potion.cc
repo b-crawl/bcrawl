fa29b56b6e4536090b9309edb308b806868e51cd
bhauth <b@name.com>
2020-12-31 02:48:01 -0700

    don't warn about tree form in miasma now that it's immune as an unbreathing form (but death drake breath still slows from the initial hit)

--- crawl-ref/source/potion.cc
+++ crawl-ref/source/potion.cc
@@ -765,7 +765,7 @@ public:
             const cloud_type cloud = cloud_type_at(you.pos());
             if (is_damaging_cloud(cloud, false)
                 // Tree form is immune to these two.
-                && cloud != CLOUD_MEPHITIC && cloud != CLOUD_POISON
+                && cloud != CLOUD_MEPHITIC && cloud != CLOUD_POISON && cloud != CLOUD_MIASMA
                 && !yesno(make_stringf("Really become a tree while standing in "
                                        "a cloud of %s?",
                                        cloud_type_name(cloud).c_str()).c_str(),

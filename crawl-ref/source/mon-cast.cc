76029f0f6268d21a3be4ff5be3dd00d377240e04
Edgar A. Bering IV <trizor@gmail.com>
2021-01-11 00:00:49 +0200

    Fix monster tracers for Call Down Damnation
    The tracer behavior was inadvertently changed in 2f2a4d8ed992e13 which
    refactored CALL_DOWN_DAMNATION into a zap. In doing so no longer failed
    to set the explosion size for the tracer.
    
    Closes #1678

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -1381,7 +1381,6 @@ bolt mons_spell_beam(const monster* mons, spell_type spell_cast, int power,
     case SPELL_NOXIOUS_CLOUD:
     case SPELL_POISONOUS_CLOUD:
     case SPELL_THORN_VOLLEY:
-    case SPELL_CALL_DOWN_DAMNATION:
     case SPELL_HURL_DAMNATION:
     case SPELL_SPIT_POISON:
     case SPELL_MIASMA_BREATH:
@@ -1421,6 +1420,11 @@ bolt mons_spell_beam(const monster* mons, spell_type spell_cast, int power,
     case SPELL_FIRE_STORM:
         setup_fire_storm(mons, power / 2, beam);
         beam.foe_ratio = random_range(40, 55);
+        break;
+
+    case SPELL_CALL_DOWN_DAMNATION: // Set explosion size for tracer
+        zappy(spell_to_zap(real_spell), power, true, beam);
+        beam.ex_size = 1;
         break;
 
     case SPELL_MEPHITIC_CLOUD:

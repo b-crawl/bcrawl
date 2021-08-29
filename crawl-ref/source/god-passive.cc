9b8b13bfb8c3a2a0b400a87193834750ddbac90f
bhauth <b@name.com>
2021-08-29 05:42:38 -0600

    rework Ash skill boost: - remove hard piety breakpoints - remove dependence on aptitude

--- crawl-ref/source/god-passive.cc
+++ crawl-ref/source/god-passive.cc
@@ -935,8 +935,9 @@ unsigned int ash_skill_point_boost(skill_type sk, int scaled_skill)
 {
     unsigned int skill_points = 0;
 
-    skill_points += (you.skill_boost[sk] * 2 + 1) * (piety_rank() + 1)
-                    * max(scaled_skill, 1) * species_apt_factor(sk);
+    int skill = max(scaled_skill, 1);
+    skill_points += (you.skill_boost[sk] * 2 + 1) * isqrt(skill * skill * min(161, you.piety));
+
     return skill_points;
 }
 

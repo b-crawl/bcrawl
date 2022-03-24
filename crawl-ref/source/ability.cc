dabd4df8aefe2d302d8b1b7933e3e25d84b91dcf
Kate <601195+semitonal@users.noreply.github.com>
2022-03-24 18:56:00 +0000

    Don't consider avatars valid targets for Cleansing Flame (surr)
    Prevents battlespheres/spectral weapons from giving prompts when activating
    Cleansing Flame, since the effect itself already ignores them.

--- crawl-ref/source/ability.cc
+++ crawl-ref/source/ability.cc
@@ -1796,7 +1796,9 @@ static bool _abort_if_stationary()
 
 static bool _cleansing_flame_affects(const actor *act)
 {
-    return act->res_holy_energy() < 3;
+    return act->res_holy_energy() < 3
+           && (!act->is_monster()
+               || !always_shoot_through_monster(&you, *act->as_monster()));
 }
 
 /*

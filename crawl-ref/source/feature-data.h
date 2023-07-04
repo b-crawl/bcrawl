efbb26261912d2b45775ec4fc49b2c358303684b
bhauth <b@name.com>
2023-07-04 17:42:18 -0600

    update descriptions

--- crawl-ref/source/feature-data.h
+++ crawl-ref/source/feature-data.h
@@ -453,7 +453,7 @@ PORTAL_EXIT(DNGN_EXIT_DESOLATION, "gate leading back out of this place", "exit_d
     YELLOW, RED, YELLOW, YELLOW, YELLOW,
     (FFT_NOTABLE | FFT_EXAMINE_HINT), MF_STAIR_BRANCH,
 },
-BRANCH_EXIT(DNGN_EXIT_SLIME, "staircase back to the Lair", "exit_slime_pits"),
+BRANCH_EXIT(DNGN_EXIT_SLIME, "staircase back to the Orcish Mines", "exit_slime_pits"),
 
 {
     DNGN_ENTER_ORC, "staircase to the Orcish Mines", "enter_orcish_mines",
@@ -486,7 +486,7 @@ BRANCH_ENTRANCE(DNGN_ENTER_SNAKE, "staircase to the Snake Pit", "enter_snake_pit
 BRANCH_EXIT(DNGN_EXIT_SNAKE, "staircase back to the Lair", "exit_snake_pit"),
 
 BRANCH_ENTRANCE(DNGN_ENTER_ELF, "staircase to the Elven Halls", "enter_elven_halls"),
-BRANCH_EXIT(DNGN_EXIT_ELF, "staircase back to the Mines", "exit_elven_halls"),
+BRANCH_EXIT(DNGN_EXIT_ELF, "staircase back to the Lair", "exit_elven_halls"),
 
 BRANCH_ENTRANCE(DNGN_ENTER_TOMB, "staircase to the Tomb", "enter_tomb"),
 BRANCH_EXIT(DNGN_EXIT_TOMB, "staircase back to the Crypt", "exit_tomb"),

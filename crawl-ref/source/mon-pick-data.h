043e098b02ce200954e8adef9a4e0a1ab32bc88b
bhauth <b@name.com>
2023-07-04 17:47:32 -0600

    Orc: add rare slime creatures
    flavor

--- crawl-ref/source/mon-pick-data.h
+++ crawl-ref/source/mon-pick-data.h
@@ -164,6 +164,7 @@ static const pop_entry pop_orc[] =
   {  1,  4,   40, FLAT, MONS_TROLL },
   {  1,  4,   12, FLAT, MONS_CYCLOPS },
   {  1,  4,    4, FLAT, MONS_ETTIN },
+  {  1,  4,    4, FLAT, MONS_SLIME_CREATURE },
   { 0,0,0,FLAT,MONS_0 }
 };
 

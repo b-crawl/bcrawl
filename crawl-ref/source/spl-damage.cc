629c652157101323c3c77b48cc5581844d2ea4c3
bhauth <b@bhauth>
2024-10-23 18:21:29 -0600

    merge [Fix Ignition exploding on projectiles]

--- crawl-ref/source/spl-damage.cc
+++ crawl-ref/source/spl-damage.cc
@@ -1773,7 +1773,8 @@ spret cast_ignition(const actor *agent, int pow, bool fail)
         if (ai->is_monster()
             && !ai->as_monster()->wont_attack()
             && !mons_is_firewood(*ai->as_monster())
-            && !mons_is_tentacle_segment(ai->as_monster()->type))
+            && !mons_is_tentacle_segment(ai->as_monster()->type)
+            && !mons_is_projectile(*ai->as_monster()))
         {
             blast_sources.push_back(ai->position);
         }
